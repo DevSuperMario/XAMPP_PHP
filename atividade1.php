bc00574dc08db942a3bdab34cbfed8ff78c65ed5
DevSuperMario <devmariomacedo@gmail.com>
2024-09-13 20:56:01 -0300

    Tratamento de forms

--- atividade1.php
+++ atividade1.php
@@ -10,30 +10,43 @@
 
 <body>
     <div class="container">
-    <h1>Digite seus dados</h1>
-    <form class="row g-3">                                                                                              
-        <div class="col-md-12">
-            <label for="inputEmail4" class="form-label">Nome</label>
-            <input type="text" class="form-control">
-        </div>
-        <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Sexo</label>
-            <select id="inputState" class="form-select">
-                <option selected>Selecione</option>
-                <option value="Masculino">Masculino</option>
-                <option value="Feminino">Feminino</option>
-            </select>
-        </div>
-        <div class="col-md-12">
-            <label for="inputEmail4" class="form-label">Idade</label>
-            <input type="number" class="form-control">
-        </div>
-        
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Salvar</button>
-        </div>
-</div>
+        <h1>Digite seus dados</h1>
+        <form class="row g-3">
+            <div class="col-md-12">
+                <label for="inputEmail4" class="form-label">Nome</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Sexo</label>
+                <select id="inputState" class="form-select">
+                    <option selected>Selecione</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                </select>
+            </div>
+            <div class="col-md-12">
+                <label for="inputEmail4" class="form-label">Idade</label>
+                <input type="number" class="form-control">
+            </div>
+
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+    </div>
     </form>
+
+    <?php
+
+    if (!empty($_GET['nome'])) {
+
+        if ($_GET['sexo'] == "Feminino" and $_GET['idade'] >= 25) {
+            echo "div id = 'aceita' > ACEITA </div>";
+        } else {
+            echo "NAO ACEITA";
+        }
+    }
+    ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
